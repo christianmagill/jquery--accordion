7288e0eb95358c35ee16c1908390b3bdd24bc7b1
Christian Magill <christianmagill@gmail.com>
2016-05-22 22:09:24 -0400

    Initial commit

---
+++ js/accordion.js
@@ -0,0 +1,30 @@
+(function($){
+
+    $.fn.accordion = function (options) {
+
+        var settings = $.extend({
+            // Defaults
+            slideSpeed: 'fast',
+            itemClass: 'accordion__item',
+            titleClass: 'accordion__item-title',
+            contentClass: 'accordion__item-content',
+            itemActiveClass: 'is-active' 
+        }, options);
+
+        var titleClick = function (e) {
+            e.preventDefault();
+            $(this).parent()
+                .toggleClass(settings.itemActiveClass)
+                .find('> .' + settings.contentClass)
+                .slideToggle(settings.slideSpeed);
+        };
+
+        return this.each(function () {
+            var titleSelector = '> .' + settings.itemClass + ' > .' + settings.titleClass; // allows for accordions in accordions
+            $(this).find('.' + settings.contentClass).hide();
+            $(this).find(titleSelector).click(titleClick);
+        });
+
+    };
+
+})(jQuery);
